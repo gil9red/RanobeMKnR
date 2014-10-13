0e6f43152e1dd1fecf29238f4acb0a4f04d6f664
Ilya Petrash <gil9red@gmail.com>
2014-10-14 01:45:03 +0600

    Информация о ранобе: название, автор, серия, информация о томах, и т.п. сохранена в файл _ranobe_/ranobe_info.
    Signed-off-by: Ilya Petrash <gil9red@gmail.com>

--- download_ranobe_mknr.py
+++ download_ranobe_mknr.py
@@ -1,14 +1,12 @@
 __author__ = 'ipetrash'
 
 
-# from grab import Grab
-# from os import makedirs
-# import os.path
-
-
 import get_ranobe_info
 import get_volume_info
+import json
 from urllib.parse import urljoin
+import os.path
+from os import makedirs
 
 
 if __name__ == '__main__':
@@ -19,28 +17,55 @@ if __name__ == '__main__':
     annotation = ranobe["annotation"]
     list_volumes = ranobe["list_volumes"]
 
-    # # В папке с скриптом создаем папку c названием name_ranobe
-    # if not os.path.exists(name_ranobe):
-    #     makedirs(name_ranobe)
+    # В папке с скриптом создаем папку c названием name_ranobe
+    dir_ranobe = "_ranobe_"
+    if not os.path.exists(dir_ranobe):
+        makedirs(dir_ranobe)
+
+    author = None
+    series = None
+    illustrator = None
 
     print("Название: '{}'".format(name_ranobe))
     print("\nАннотации:\n'{}'".format(annotation))
     print("\nТома ({}):".format(len(list_volumes)))
 
+    volumes = list()
     for n, v in enumerate(list_volumes, 1):
         print("Глава {}:".format(n))
 
-        # Соединение адреса к главной странице ранобе и относительной ссылки к тому
+        # Соединение адреса к главной странице ранобе и относительной ссылки к тОму
         url_volume = urljoin(url, v.attr('href'))
 
         volume_info = get_volume_info.volume_info(url_volume, url)
         if volume_info:
+            # Номер тома в серии
+            volume_info["number"] = n
+
+            # Добавляем том к списку томов
+            volumes.append(volume_info)
+
+            # # Создание папки тома ранобе
+            # volume_name = volume_info.get("name").replace(':', '.')
+            # dir_volume_ranobe = os.path.join(dir_ranobe, volume_name)
+            # if not os.path.exists(dir_volume_ranobe):
+            #     makedirs(dir_volume_ranobe)
+
+            if not author:
+                author = volume_info.get("author")
+
+            if not series:
+                series = volume_info.get("series")
+
+            if not illustrator:
+                illustrator = volume_info.get("illustrator")
+
             print("  Адрес тома: {}".format(url_volume))
             print("  ALL: {}".format(volume_info))
             print("    Название:    {}".format(volume_info.get("name")))
-            print("    Серия:       {}".format(volume_info.get("series")))
-            print("    Автор:       {}".format(volume_info.get("author")))
-            print("    Иллюстратор: {}".format(volume_info.get("illustrator")))
+            print("    Серия:       {}".format(series))
+            print("    Автор:       {}".format(author))
+            print("    Иллюстратор: {}".format(illustrator))
             print("    ISBN:        {}".format(volume_info.get("ISBN")))
             print("    Обложка:     {}".format(volume_info.get("url_cover")))
             print("    Содержание:")
@@ -55,5 +80,18 @@ if __name__ == '__main__':
             print("        Запоздавший шедевр: {}".format(volume_info.get("a2")))
         else:
             print("Неудача с томом: {}".format(url_volume))
-
         print()
+
+    # Создадим файл, содержащий описание к ранобе: название, автор, аннотацию и т.п.
+    ranobe_info_path = os.path.join(dir_ranobe, 'ranobe_info')
+    with open(ranobe_info_path, mode='w', encoding='utf8') as f:
+        dump_data = {
+            "name": name_ranobe,
+            "author": author,
+            "illustrator": illustrator,
+            "series": series,
+            "annotation": annotation,
+            "url": url,
+            "volumes": volumes,
+        }
+        json.dump(dump_data, f, ensure_ascii=False, sort_keys=True, indent=4)
