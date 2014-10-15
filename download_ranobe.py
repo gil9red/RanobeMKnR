d46dcb9171d0ce99b8f097fe7b1aacb393cfaffb
Ilya Petrash <gil9red@gmail.com>
2014-10-16 00:09:58 +0600

    Для каждого тома ранобе , по пути (dir) в файле ranobe_info, созданы папки.
    Signed-off-by: Ilya Petrash <gil9red@gmail.com>

--- download_ranobe.py
+++ download_ranobe.py
@@ -1,19 +1,20 @@
 __author__ = 'ipetrash'
 
-
 """Скрипт, используя данные в _ranobe_/ranobe_info.json (которые получает и сохраняет
 скрипт generate_info_ranobe.py), качает с сайта для дальнейшей работы с ними."""
 
-
-import generate_info_ranobe
 import os.path
+from os import makedirs
 import sys
 import json
+
+import generate_info_ranobe
 
 
 if __name__ == '__main__':
     # Путь к файлу с инфой об ранобе
     ranobe_info_path = generate_info_ranobe.RANOBE_INFO_PATH
+    ranobe_dir = generate_info_ranobe.DIR_RANOBE
 
     try:
         # Если файла по такому пути не существует, завершаем работу скрипта
@@ -31,13 +32,27 @@ if __name__ == '__main__':
             except ValueError as err:
                 raise Exception("Файл {} испорчен: {}".format(ranobe_info_path, err))
 
-
         print(ranobe_info)
-        print("\nНазвание: '{}'".format(ranobe_info.get("name")))
-        print("Автор: '{}'".format(ranobe_info.get("author")))
-        print("Иллюстратор: '{}'".format(ranobe_info.get("illustrator")))
-        print("Серия: '{}'".format(ranobe_info.get("series")))
-        print("Аннотации:\n'{}'".format(ranobe_info.get("annotation")))
+
+        # # Создадим папку с таким же названием, как у ранобе
+        # name = ranobe_info.get("name")
+        # path_dir_ranobe = os.path.join(ranobe_dir, name)
+        # if not os.path.exists(path_dir_ranobe):
+        # makedirs(path_dir_ranobe)
+
+        # Перебор всех томов ранобе
+        volumes = ranobe_info.get("volumes")
+        for volume in volumes:
+            path_dir_volume = volume.get("dir")
+            if not os.path.exists(path_dir_volume):
+                makedirs(path_dir_volume)
+
+
+                # print("\nНазвание: '{}'".format(name))
+                # print("Автор: '{}'".format(ranobe_info.get("author")))
+                # print("Иллюстратор: '{}'".format(ranobe_info.get("illustrator")))
+                # print("Серия: '{}'".format(ranobe_info.get("series")))
+                # print("Аннотации:\n'{}'".format(ranobe_info.get("annotation")))
 
 
     except Exception as err:
