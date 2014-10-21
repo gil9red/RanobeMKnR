210337190922e6e9f11e3fab2bdfbed1eab462cc
Ilya Petrash <gil9red@gmail.com>
2014-10-22 01:50:33 +0600

    Добавление к генератору названия ранобе (book-title) и аннотации (annotation).
    Signed-off-by: Ilya Petrash <gil9red@gmail.com>

--- compile_ranobe.py
+++ compile_ranobe.py
@@ -1,10 +1,13 @@
+from get_ranobe_info import ranobe_info
+
 __author__ = 'ipetrash'
 
 """Скрипт, используя данные скриптов get_ranobe_info и download_ranobe,
 генерирует файл в формате fb2."""
 
 import os.path
-import sys
+# import sys
+import json
 
 import generate_info_ranobe
 
@@ -16,9 +19,15 @@ if __name__ == '__main__':
     # Путь к папке с генерированной информацией
     ranobe_dir = generate_info_ranobe.DIR_RANOBE
 
-    if not os.path.exists(ranobe_dir):
-        print('{} не существует.'.format(ranobe_dir))
-        sys.exit(-1)
+    # Путь к файлу с инфой об ранобе
+    ranobe_info_path = generate_info_ranobe.RANOBE_INFO_PATH
+
+    ranobe_info = None
+
+    # Открываем файл в режиме чтения
+    with open(ranobe_info_path, mode='r', encoding='utf8') as f:
+        # Десериализация данных в объекты python'а
+        ranobe_info = json.load(f)
 
     # TODO: имя файла с ранобе нужно такое же как и название ранобе
     # Название файла ранобе
@@ -32,5 +41,25 @@ if __name__ == '__main__':
         f.write('<?xml version="1.0" encoding="UTF-8"?>' + '\n')
         f.write('<FictionBook xmlns="http://www.gribuser.ru/xml/fictionbook/2.0" '
                 'xmlns:xlink="http://www.w3.org/1999/xlink">' + '\n')
-        f.write('<body>' + '<p>' + 'Hello мир!' + '</p>' + '</body>')
+        f.write('<description>')
+        f.write('<title-info>')
+
+        f.write('<book-title>' + ranobe_info['name'] + '</book-title>')
+
+        f.write('<author>')
+        # TODO: рефакторить получение имени и фамилии
+        f.write('<first-name>' + ranobe_info['author'].split(' ')[0] + '</first-name>')
+        f.write('<last-name>' + ranobe_info['author'].split(' ')[1] + '</last-name>')
+        f.write('</author>')
+
+        f.write('<annotation>')
+        annotation = ''
+        for line in ranobe_info['annotation'].split('\n'):
+            annotation += '<p>{}</p>'.format(line)
+        f.write(annotation)
+        f.write('</annotation>')
+
+        f.write('</title-info>')
+        f.write('</description>')
+        # f.write('<body>' + '<p>' + 'Hello мир!' + '</p>' + '</body>')
         f.write('</FictionBook>')
