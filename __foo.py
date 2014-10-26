1d8d2094709b74fc1ce1f76d16fbd1da9000a28c
Ilya Petrash <gil9red@gmail.com>
2014-10-27 00:45:00 +0600

    Скрипт для получения содержания томом с учетом двух уровней вложенности: главы и подглавы
    Signed-off-by: Ilya Petrash <gil9red@gmail.com>

---
+++ __foo.py
@@ -0,0 +1,38 @@
+__author__ = 'ipetrash'
+
+from grab import Grab
+from urllib.parse import urljoin
+
+# Скрипт для получения содержания томом с учетом двух уровней вложенности:
+# главы и подглавы
+
+url = 'http://ruranobe.ru/r/mknr/v5'
+
+g = Grab()
+g.go(url)
+
+# Получение списка глав из оглавления
+content = g.doc.select('//div[@id="index"]/ol/li')
+
+for i, c in enumerate(content, 1):
+    # Проверяем на подсписок
+    if c.node.find('ol') is not None:
+        # Вывод названия элемента
+        print("{}. {}:\n".format(i, c.node.text.strip()), end='')
+
+        # И вывод его подсписка
+        for j, ch in enumerate(content.select('ol/li/a'), 1):
+            url_ch = urljoin(url, ch.attr('href'))
+            print("  {}. '{}': {}".format(j, ch.text(), url_ch))
+    else:
+        url_ch = urljoin(url, c.select('a').attr('href'))
+        print("{}. '{}': {}".format(i, c.text(), url_ch))
+
+
+# g = Grab()
+# g.go('http://ruranobe.ru/r/mknr/v1/ch1')
+#
+# # Получение основного контекста, имеющий номер главы и
+# content_text = g.doc.select('//div[@id="mw-content-text"]')
+#
+# print(content_text.html())
