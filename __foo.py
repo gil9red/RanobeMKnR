2bfaa5f79d5b83c4ea354ec37febe632a64bbc4f
Ilya Petrash <gil9red@gmail.com>
2014-10-29 22:20:40 +0600

    Удален ненужный код.
    Signed-off-by: Ilya Petrash <gil9red@gmail.com>

--- __foo.py
+++ __foo.py
@@ -1,56 +1,13 @@
 __author__ = 'ipetrash'
 
-from urllib.parse import urljoin
-
 from grab import Grab
 
-
-def get_chapters(grab_url_volume):
-    """Функция возвращает содержание тома с учетом двух уровней
-    вложенности: главы и подглавы"""
-
-    # Получение списка глав из оглавления
-    content = grab_url_volume.doc.select('//div[@id="index"]/ol/li')
-
-    chapters = list()
-
-    for c in content:
-        # Проверяем на подсписок
-        if c.node.find('ol') is not None:
-            sub_chapters = list()
-            chapters.append((c.node.text.strip(), sub_chapters))
-
-            # Подсписок:
-            for ch in content.select('ol/li/a'):
-                url_ch = urljoin(url, ch.attr('href'))
-                sub_chapters.append((ch.text(), url_ch))
-        else:
-            url_ch = urljoin(url, c.select('a').attr('href'))
-            chapters.append((c.text(), url_ch))
-
-    return chapters
-
-
 if __name__ == '__main__':
-    url = 'http://ruranobe.ru/r/mknr/v5'
+    url = 'http://ruranobe.ru/r/mknr/v1'
 
     g = Grab()
     g.go(url)
 
-    chapters = get_chapters(g)
-
-    for ch in chapters:
-        name, url = ch
-        if not isinstance(url, list):
-            print("'{}': {}".format(name, url))
-        else:
-            print("'{}':".format(name))
-            for sub_ch in url:
-                sub_name, sub_url = sub_ch
-                print("    '{}': {}".format(sub_name, sub_url))
-
-
-                # # Получение основного контекста, имеющий номер главы и
-                # content_text = g.doc.select('//div[@id="mw-content-text"]')
-                #
-                # print(content_text.html())
+    # Получение основного контекста, имеющий номер главы и
+    content_text = g.doc.select('//div[@id="mw-content-text"]')
+    print(content_text.html())
