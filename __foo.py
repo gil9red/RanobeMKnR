ab013b1bbf183b8a05b934952fd81016fcde96f3
Ilya Petrash <gil9red@gmail.com>
2014-11-01 00:37:03 +0600

    Обновлено.
    Signed-off-by: Ilya Petrash <gil9red@gmail.com>

--- __foo.py
+++ __foo.py
@@ -3,11 +3,17 @@ __author__ = 'ipetrash'
 from grab import Grab
 
 if __name__ == '__main__':
-    url = 'http://ruranobe.ru/r/mknr/v1'
+    # url = 'http://ruranobe.ru/r/mknr/v1/ch1'
 
-    g = Grab()
-    g.go(url)
+    data = None
+    with open('_ranobe_\ch1.html', encoding='utf8') as f:
+        data = f.read()
+
+    g = Grab(data)
 
     # Получение основного контекста, имеющий номер главы и
-    content_text = g.doc.select('//div[@id="mw-content-text"]')
-    print(content_text.html())
+    # content_text = g.doc.select('//div[@id="mw-content-text"]')
+    # print(content_text.html())
+
+    print(g.doc.select('//ol[@class="references"]').count())
+    print(g.doc.select('//ol[@class="references"]/li').count())
